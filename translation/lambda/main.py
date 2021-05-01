a3259ba4d1dfb1e1dd10041c755c8b5032faf51a
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2021-05-01 18:26:17 +0530

    Update main.py

--- translation/lambda/main.py
+++ translation/lambda/main.py
@@ -3,6 +3,6 @@ from src.easy_nmt import translate_records
 
 def lambda_handler(event, context):
     try:
-        return translate_records(event)
+        return translate_records(**event)
     except Exception as e:
         raise
