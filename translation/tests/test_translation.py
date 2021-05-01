bf3d3fab9de458a8545ebb90b1536bbcefff2191
Pratik Bhavsar <pratik.a.bhavsar@gmail.com>
2021-05-01 19:34:40 +0530

    Update test_translation.py

--- translation/tests/test_translation.py
+++ translation/tests/test_translation.py
@@ -10,32 +10,28 @@ requests = {
         "id": "12",
         "text": "I will never download your app will steal your bank information and authorizes a subscription you never request",
     },
+     {
+      "id": "13",
+      "text": "Je ne téléchargerai jamais votre application volera"
+    }
 ],
     "target_lang": "en",
     "source_lang": "ROMANCE"
 }
 
-expected_response = {
-    
-    "records": [
-        {
-            "id": "11",
-            "translated": "I'll never download your app again. It steals your bank information and authorizes a subscription that you never request.",
-            "source_lang": "es",
-        },
-        {
-            "id": "12",
-            "translated": "",
-            "source_lang": "en",
-        },
-    ]
-}
+expected_response = {'target_lang': 'en', 
+                     'records': [
+                         {'id': '11', 'translated': "I'll never download your app again, steal your bank information and authorize a subscription that you never ask for.", 'source_lang': 'es'}, 
+                         {'id': '12', 'translated': '', 'source_lang': 'en'}, 
+                         {'id': '13', 'translated': 'I will never download your app will fly', 'source_lang': 'fr'}]
+                    }
 
 
 
 def test_response():
     global expected_response
     response = translate_records(**requests)
+    del response["translation_time"]
     assert expected_response == response
 
 
